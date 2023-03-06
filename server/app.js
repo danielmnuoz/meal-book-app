531181a557738f895bca418ed6eab14af6ec10ca
Daniel Munoz <dmunozl@clemson.edu>
2023-03-05 21:21:30 -0500

    Some UI Changes! Made nav/home component and a lil backend

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const models = require('./models');
 // const app = express();
 // app.listen(3000, () => console.log("server Connected on port 3000"));
 
