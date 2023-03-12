9820a28f960f8b059af67838c201b728815abc1f
Daniel Munoz <dmunozl@clemson.edu>
2023-03-12 19:13:47 -0400

    test fancy login ui with sign up

---
+++ ui/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const signUpButton = document.getElementById('sign-up-btn') as HTMLButtonElement;
+    const signInButton = document.getElementById('sign-in-btn') as HTMLButtonElement;
+    const container = document.querySelector('.container') as HTMLElement;
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("sign-up-mode");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("sign-up-mode");
+    });
+  }
+
+}
