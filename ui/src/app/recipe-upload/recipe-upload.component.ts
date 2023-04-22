71e77d1c832e3e7b524fbfddc2cdf1ca66e52558
chrisrod622922 <chrisrod9222@icloud.com>
2023-04-22 13:34:15 -0400

    Updated recipe-upload.component.ts

--- ui/src/app/recipe-upload/recipe-upload.component.ts
+++ ui/src/app/recipe-upload/recipe-upload.component.ts
@@ -6,25 +6,27 @@ import { NgForm } from '@angular/forms';
   templateUrl: './recipe-upload.component.html',
   styleUrls: ['./recipe-upload.component.css']
 })
+
+// Create Recipe class
+export class Recipe {
+  constructor(
+    public name: string,
+    public ingredients: string,
+    public prep_time: number,
+    public cook_time: number,
+    public yield_amount: number,
+    public yield_unit: string,
+    public prep_inst: string,
+    public cook_inst: string
+  ) {}
+}
+
 export class RecipeUploadComponent {
   http: any;
-
-  // Sample Recipe object
-  Recipe: { name: string, ingredients: string, prep_time: number, cook_time: number,
-            yield: number, yield_unit: string, prep_inst: string, cook_inst: string } = {
-    name: "Sample Recipe Title",
-    ingredients: "Sample ingredients here",
-    prep_time: 20,
-    cook_time: 15,
-    yield: 2,
-    yield_unit: "lbs",
-    prep_inst: "Prep instructions here",
-    cook_inst: "Cooking instructions here"
-  };
   
   onSubmit(form: NgForm) {
     // Create a new recipe object
-    const recipe = new this.Recipe(
+    const recipe = new Recipe(
       form.value.Name,
       form.value.Ingredients,
       form.value.PrepTimeMin,
@@ -36,9 +38,7 @@ export class RecipeUploadComponent {
     );
   
     // Send the recipe object to the server
-    TODO: // complete request
-    // Pretty sure API url needs to be below
-    this.http.post('mongodb+srv://dmunozl:4720adminpassword@4720-cluster.cld7ug6.mongodb.net/?retryWrites=true&w=majority', recipe)
+    this.http.post('https://106bf2a5-bfea-4597-92d1-97c02590d285.mock.pstmn.io/recipes', recipe)
       .subscribe(
         (response: any) => {
           console.log('Recipe saved successfully!');
