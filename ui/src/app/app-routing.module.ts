7e4fbc87044b9721bf10f54843759ddbf826442e
chrisrod622922 <chrisrod9222@icloud.com>
2023-04-22 14:02:18 -0400

    Added recipe-upload path to app-routing

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './user-dashboard/dashboard.component';
+import { RecipeUploadComponent } from './recipe-upload/recipe-upload.component';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'user-dashboard', component: DashboardComponent}
+  { path: 'user-dashboard', component: DashboardComponent},
+  { path: 'recipe-upload', component: RecipeUploadComponent}
 ];
 
 @NgModule({
